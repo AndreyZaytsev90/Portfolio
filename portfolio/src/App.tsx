b19ceac7398f70bfa01ce62f4249b8cafc76781c
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-04-12 14:58:40 +0300

    Change files

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Header from "./header/Header";
 import Main from "./main/Main";
 import Skills from "./skills/Skills";
+import Projects from "./projects/Projects";
 
 type AppPropsType = {
 
@@ -14,6 +15,7 @@ function App(props: AppPropsType) {
             <Header/>
             <Main/>
             <Skills /*id={props.id}*//>
+            <Projects/>
         </div>
     );
 }
--- portfolio/src/skills/Skills.tsx
+++ portfolio/src/skills/Skills.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import style from './Skills.module.css'
 import styleContainer from './../common/styles/Container.module.css'
 import Skill from "./skill/Skill";
-import {v1} from "uuid";
 
 type SkillsPropsType = {
 
@@ -15,8 +14,8 @@ const Skills = (props: SkillsPropsType) => {
                 <h2 className={style.title}>My skills</h2>
                 <div className={style.skills}>
                     <Skill  title={"HTML/CSS"} description={"Semantic"}/>
-                    <Skill  title={"JavaScript"} description={""}/>
-                    <Skill  title={"React"} description={""}/>
+                    <Skill  title={"JavaScript/TypeScript"} description={""}/>
+                    <Skill  title={"React/Redux"} description={""}/>
                 </div>
             </div>
         </div>
