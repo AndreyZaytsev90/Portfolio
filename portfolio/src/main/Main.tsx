320b3ee3f183424583882b9537b07df53a1ecab7
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-09-20 20:58:54 +0300

    Added photo

--- portfolio/src/main/Main.tsx
+++ portfolio/src/main/Main.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import style from './Main.module.css'
 import styleContainer from './../common/styles/Container.module.css'
+import Photo from '../img/photo_2.jpg'
 
 const Main = () => {
-    return (
-        <div className={style.mainBlock}>
-            <div className={styleContainer.container}>
-                <div className={style.greeting}>
-                    <span>Hi There</span>
-                    <h1>I am Andrey Zaytsev</h1>
-                    <p>Frontend Developer</p>
-                </div>
-                <div className={style.photo}></div>
-            </div>
+  return (
+    <div className={style.mainBlock}>
+      <div className={styleContainer.container}>
+        <div className={style.greeting}>
+          <span>Hi There</span>
+          <h1>I am Andrey Zaytsev</h1>
+          <p>Frontend Developer</p>
         </div>
-    );
+        <div>
+          <img className={style.photo} src={Photo} alt="Photo"/>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
