e52ff634105709755b7ab7570fcdc0841873bbad
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-09-26 22:44:39 +0300

    Fixed bug

--- portfolio/src/projects/Projects.tsx
+++ portfolio/src/projects/Projects.tsx
@@ -3,22 +3,22 @@ import style from './Projects.module.css'
 import styleContainer from './../common/styles/Container.module.css'
 import Project from "./project/Project";
 
-type ProjectsPropsType = {
-
-}
+type ProjectsPropsType = {}
 
 const Projects = (props: ProjectsPropsType) => {
-    return (
-        <div className={style.projectsBlock}>
-            <div className={`${styleContainer.container} ${style.projectsContainer}`}>
-                <h2 className={style.title}>My Projects</h2>
-                <div className={style.projects}>
-                    <Project title={"Social network"} description={"first project"}/>
-                    <Project title={"TodoList"} description={"second project"}/>
-                </div>
-            </div>
+  return (
+    <div className={style.projectsBlock}>
+      <div className={`${styleContainer.container} ${style.projectsContainer}`}>
+        <div className={style.title}>
+          <h2>My Projects</h2>
         </div>
-    );
+        <div className={style.projects}>
+          <Project title={"Social network"} description={"first project"}/>
+          <Project title={"TodoList"} description={"second project"}/>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
