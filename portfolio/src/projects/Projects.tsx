6e6a6e3300ef5875fc284e2fc4a6fdfe5219b4be
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-09-27 22:53:57 +0300

    Updated file

--- portfolio/src/projects/Projects.tsx
+++ portfolio/src/projects/Projects.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import style from './Projects.module.css'
 import styleContainer from './../common/styles/Container.module.css'
 import Project from "./project/Project";
+import Title from "../common/components/title/Title";
 
 type ProjectsPropsType = {}
 
@@ -9,9 +10,7 @@ const Projects = (props: ProjectsPropsType) => {
   return (
     <div className={style.projectsBlock}>
       <div className={`${styleContainer.container} ${style.projectsContainer}`}>
-        <div className={style.title}>
-          <h2>My Projects</h2>
-        </div>
+        <Title textTitle="My Projects"/>
         <div className={style.projects}>
           <Project title={"Social network"} description={"first project"}/>
           <Project title={"TodoList"} description={"second project"}/>
--- portfolio/src/skills/Skills.tsx
+++ portfolio/src/skills/Skills.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import style from './Skills.module.css'
 import styleContainer from './../common/styles/Container.module.css'
 import Skill from "./skill/Skill";
+import Title from "../common/components/title/Title";
 
 type SkillsPropsType = {
 
@@ -11,7 +12,7 @@ const Skills = (props: SkillsPropsType) => {
     return (
         <div className={style.skillsBlock}>
             <div className={`${styleContainer.container} ${style.skillsContainer}`}>
-                <h2 className={style.title}>My skills</h2>
+                <Title textTitle="My Skills"/>
                 <div className={style.skills}>
                     <Skill  title={"HTML/CSS"} description={"Semantic HTML5 and CSS3"}/>
                     <Skill  title={"JavaScript/TypeScript"} description={"ES6"}/>
